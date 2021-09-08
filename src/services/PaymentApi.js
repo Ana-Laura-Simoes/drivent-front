9b1f46332e9d2f7dfe3662395b78d6023a021cd6
Rodrigo Schmidt <rodrigodcsee@gmail.com>
2021-09-08 16:07:45 -0300

    fix: for deployment in vercel

--- src/services/PaymentApi.js
+++ src/services/PaymentApi.js
@@ -1,6 +1,5 @@
 import AuthenticatedApi from "./AuthenticatedApi";
 import api from "./api";
-import AuthenticatedApi from "./AuthenticatedApi";
 
 export default class PaymentApi extends AuthenticatedApi {
   static createPayment(body) {
