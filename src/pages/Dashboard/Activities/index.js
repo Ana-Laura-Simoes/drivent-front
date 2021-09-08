31768d5d423ab570d93db0f814d30ebf38479b29
hamandakuntz <hamanda.monteiro@hotmail.com>
2021-09-08 14:04:49 -0300

    feat: message don't need to choose activity to online tickets added

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,35 @@
+import { useEffect, useState, useContext } from "react";
+import useApi from "./../../../hooks/useApi";
+import { toast } from "react-toastify";
+import DontNeedToChooseActivitiesMessage from "./DontNeedToChooseActivitiesMessage";
+
 export default function Activities() {
-  return "Atividades: Em breve!";
+  const { payment } = useApi();
+  const [isAnOnlineTicket, setIsAnOnlineTicket] = useState(false);
+
+  useEffect(() => {
+    payment
+      .getPayment()
+      .then((response) => {
+        console.log(response.data);
+        if (response.data.type === "Online") setIsAnOnlineTicket(true);
+      })
+      .catch((error) => {
+        if (error.response?.data?.details) {
+          for (const detail of error.response.data.details) {
+            toast(detail);
+          }
+        } else {
+          toast("Não foi possível carregar");
+        }
+        /* eslint-disable-next-line no-console */
+        console.log(error);
+      });
+  }, []);
+
+  if (isAnOnlineTicket) {
+    return <DontNeedToChooseActivitiesMessage />;
+  } else {
+    return "Atividades: Em breve!";
+  }
 }
--- src/services/PaymentApi.js
+++ src/services/PaymentApi.js
@@ -1,7 +1,16 @@
 import api from "./api";
+import AuthenticatedApi from "./AuthenticatedApi";
 
-export default class PaymentApi {
+export default class PaymentApi extends AuthenticatedApi {
   static createPayment(body) {
     return api.post("/payment", body);
+  }
+
+  getPayment() {
+    return api.get("/payment", {
+      headers: {
+        ...this.getAuthorizationHeader()
+      }
+    });
   }
 }
